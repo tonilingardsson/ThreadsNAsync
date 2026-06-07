bb10d1ba40ec7af8650164ac8a1cb469c2c81033
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-07 21:37:55 +0200

    Added car model with default values

--- ThreadsNAsync/Car.cs
+++ ThreadsNAsync/Car.cs
@@ -1,10 +1,18 @@
-﻿namespace CarRace;
-
-public class Car
+﻿namespace ThreadsNAsync
 {
-    public string Name { get; set; } = "";
-    public double DistanceKm { get; set; } = 0;
-    public double SpeedKmH { get; set; } = 120;
-    public bool Finished { get; set; } = false;
-    public DateTime? FinishTime { get; set; }
+    public class Car
+    {
+        public string Name { get; set; }
+        public double DistanceKm { get; set; }
+        public int SpeedKmH { get; set; }
+        public bool Finished { get; set; }
+
+        public Car(string name)
+        {
+            Name = name;
+            DistanceKm = 0;
+            SpeedKmH = 120;
+            Finished = false;
+        }
+    }
 }
